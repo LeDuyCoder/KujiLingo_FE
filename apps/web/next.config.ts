ece96566a033157592a786770e4f76c1baa31d80
Kira-Window <duycoder544@gmail.com>
2026-09-01 15:45:22 +0700

    feat(premium): implement premium landing and pricing pages

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.4'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.transparenttextures.com',
+      }
+    ],
+  },
   async rewrites() {
     return [];
   },
